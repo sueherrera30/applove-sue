492658f7e5e0e5dbe52c5cf02ea3b832650df058
sue martinez <sueherrera30@gmail.com>
2017-04-17 22:45:20 +0000

    esta verson creo  ya hace segun lo que pedian

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,19 @@
-var boton =document.createElement("Button");
-boton.addEventListener( onclick,"cerrar");
+var tache=document.getElementsByClassName("botoncerrar")[0];
+var imagenMostrada = document.getElementById("imagenMostrada");
+var imagenes = document.getElementsByClassName("imagenes");
+var imagenAver= document.getElementById('imagen');
 
-var imagen=
+var cuantasImagenes = imagenes.length;
+for(var i=0; i< cuantasImagenes; i++){
+  imagenes[i].addEventListener("click",mostrar);
+}
+function mostrar(){
+imagenMostrada.style.display="block";
+imagenAver.src = this.src;
+}
+
+tache.addEventListener("click",cerrarImagen);
+
+function cerrarImagen(){
+imagenMostrada.style.display ="none";
+}
